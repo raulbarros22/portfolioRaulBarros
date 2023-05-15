ca985e1fd573bc9590ab911079f04dce47ca2263
Raul Barros <barrosraul22@gmail.com>
2023-05-14 22:25:17 -0300

    5to commit

--- src/app/servicios/portfolio.service.ts
+++ src/app/servicios/portfolio.service.ts
@@ -2,12 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class PortfolioService {
+  URL = 'http://localhost:8080/personas';
 
   constructor(private http:HttpClient) { }
+
+  public getPersonas(): Observable<persona>{
+    return this.http.get<persona>(this.URL+'traer/perfil');
+  }
   obtenerDatos():Observable<any> {
     /* console.log('El servicio portfolio está corriendo...'); */
     return this.http.get('./assets/data/data.json');
