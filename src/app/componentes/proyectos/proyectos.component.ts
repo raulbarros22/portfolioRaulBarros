b72adce0f04ca9951a5e5dce021cdc6c2accde94
raulbarros22 <barrosraul22@gmail.com>
2023-05-22 17:17:52 -0300

    3er commit

--- src/app/componentes/proyectos/proyectos.component.ts
+++ src/app/componentes/proyectos/proyectos.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
   styleUrls: ['./proyectos.component.css']
 })
-export class ProyectosComponent {
-
+export class ProyectosComponent implements OnInit{
+  infoProjects: any;
+  constructor(private datosPortfolio:PortfolioService){}
+  ngOnInit(): void{  
+    this.datosPortfolio.obtenerDatos().subscribe(data =>{
+    this.infoProjects=data.projects;
+    });
+  }
 }
